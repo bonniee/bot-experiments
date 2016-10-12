5321769cab15fe29acafbe8f6d53b873b0227234
Bonnie Eisenman <bonnie.eisenman@gmail.com>
2016-10-12 14:30:36 -0400

    got it

--- setSVGs.py
+++ setSVGs.py
@@ -27,7 +27,7 @@ suffix = """
 """
 
 body="""
-  <rect x="0" y="0" style="fill:#FFBBBB" width="580" height="260"/>
+  <rect x="0" y="0" style="fill:#e8efed" width="580" height="260"/>
 """
 
 CARD_HEIGHT = 60
@@ -39,58 +39,86 @@ CARD_MARGIN = 20
 
 STROKE_WIDTH = SYMBOL_WIDTH / 5.0
 
-HATCHING = """<pattern id="diagonalHatch" width="10" height="10" patternTransform="rotate(45 0 0)" patternUnits="userSpaceOnUse">
-  <line x1="0" y1="0" x2="0" y2="10" style="stroke:black; stroke-width:1" />
-</pattern>"""
+COLORS = ["66c2a5", "fc8d62", "8da0cb"]
 
-prefix += HATCHING
+HATCHING = """
+<pattern id="hatch{0}" width="4" height="4" patternTransform="rotate(45 0 0)" patternUnits="userSpaceOnUse">
+<ellipse cx="2" cy="2" rx="1.3" ry="1.3" fill="#{0}"/>
+</pattern>
+"""
 
-def drawRect(x, y, color):
+for c in COLORS:
+  prefix += HATCHING.format(c)
+
+
+def drawRect(x, y, style):
   return """
-      <rect x="{0}" y="{1}" style="fill:#{2}" width="{3}" height="{4}" stroke-width="5"/>
-  """.format(x, y, color, SYMBOL_WIDTH, SYMBOL_HEIGHT)
+      <rect x="{}" y="{}" width="{}" height="{}" {}/>
+  """.format(x, y, SYMBOL_WIDTH, SYMBOL_HEIGHT, style)
 
-def drawCircle(x, y, color):
+def drawCircle(x, y, style):
   r = SYMBOL_WIDTH / 2.0
   cx = r + x
   cy = r + y
   return """
-      <ellipse cx="{0}" cy="{1}" style="fill:#{2}" rx="{3}" ry="{3}" stroke-width="5"/>
-  """.format(cx, cy, color, r)
+      <ellipse cx="{}" cy="{}" rx="{}" ry="{}" {}/>
+  """.format(cx, cy, r, r, style)
 
-def drawTriangle(x, y, color):
+def drawTriangle(x, y, style):
   r = SYMBOL_WIDTH / 2.0
-  return """<polygon points="{} {}, {} {}, {} {}" style="fill:#{}" stroke-width="5"/>
-  """.format(x + r, y, x + SYMBOL_WIDTH, y + SYMBOL_HEIGHT, x, y + SYMBOL_HEIGHT, color)
+  return """<polygon points="{} {}, {} {}, {} {}" {}/>
+  """.format(x + r, y, x + SYMBOL_WIDTH, y + SYMBOL_HEIGHT, x, y + SYMBOL_HEIGHT, style)
 
+def style(pattern, color):
+  styleString = """stroke="#{}" stroke-width="5"
+  """.format(color)
+
+  if (pattern == 0):
+    # Solid
+    styleString += """
+      fill="#{}"
+    """.format(color)
+  elif (pattern == 1):
+    # Hollow
+    styleString += """
+      fill="transparent"
+    """
+  else:
+    # Striped
+    styleString += """
+    fill="url(#hatch{})" 
+    """.format(color)
+
+  return styleString
 
 def drawCardContents(x, y, num, color, shape, pattern):
   yOffset = y + (CARD_HEIGHT / 2.0) - (SYMBOL_HEIGHT / 2.0)
   xInterval = CARD_WIDTH / float(num + 1.0)
   xMargin = SYMBOL_WIDTH / 2.0
 
-  colorCode = "FF6666"
+  colorCode = "66c2a5"
   if (color == 1):
-    colorCode = "66FF66"
+    colorCode = "fc8d62"
   elif (color == 2):
-    colorCode = "6666FF"
+    colorCode = "8da0cb"
 
   coords = [(x + ((1 + i) * xInterval) - xMargin, yOffset) for i in range(num)]
+  styleString = style(pattern, colorCode)
 
   ret = ""
   for (a, b) in coords:
     if (shape == 0):
-      ret += drawRect(a, b, colorCode)
+      ret += drawRect(a, b, styleString)
     elif (shape == 1):
-      ret += drawCircle(a, b, colorCode)
+      ret += drawCircle(a, b, styleString)
     else:
-      ret += drawTriangle(a, b, colorCode)
+      ret += drawTriangle(a, b, styleString)
 
   return ret
 
 def mkCard(x, y):
   ret = """
-    <rect x="{0}" y="{1}" style="fill:#FFFFFF" width="120" height="60"/>
+    <rect x="{0}" y="{1}" rx="5" ry="5" style="fill:#FFFFFF" width="120" height="60"/>
   """.format(x, y)
   # ret += drawCardContents(x, y, 3, "FF2222")
   return ret
